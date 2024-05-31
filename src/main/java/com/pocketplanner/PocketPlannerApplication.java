d95061a93092072920a853a90eab6bbcb0a590eb
Kseniya Patapovich <ksuy.potapovich@bk.ru>
2024-05-31 21:07:37 +0300

    First commit "Create project"

---
+++ src/main/java/com/pocketplanner/PocketPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.pocketplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocketPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pocketplanner/PocketPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocketplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

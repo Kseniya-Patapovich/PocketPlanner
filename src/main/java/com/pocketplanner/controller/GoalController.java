e2b78285f0dec3fed91073c56034ebf7d22f694b
Kseniya Patapovich <ksuy.potapovich@bk.ru>
2024-06-01 13:03:26 +0300

    7th commit "GoalService and GoalController"

--- src/main/java/com/pocketplanner/controller/GoalController.java
+++ src/main/java/com/pocketplanner/controller/GoalController.java
@@ -1,12 +1,19 @@
 package com.pocketplanner.controller;
 
+import com.pocketplanner.exception.CustomValidException;
 import com.pocketplanner.model.Goal;
+import com.pocketplanner.model.dto.GoalCreateDto;
 import com.pocketplanner.service.GoalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,5 +49,21 @@ public class GoalController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @PostMapping("/{id}")
+    public ResponseEntity<HttpStatus> createGoal(@RequestBody GoalCreateDto goalCreateDto, @PathVariable("id") Long id, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new CustomValidException(bindingResult.getAllErrors().toString());
+        }
+        return new ResponseEntity<>(goalService.createGoal(goalCreateDto, id) ? HttpStatus.CREATED : HttpStatus.CONFLICT);
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteGoal(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(goalService.deleteGoal(id) ? HttpStatus.NO_CONTENT : HttpStatus.CONFLICT);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<HttpStatus> updateGoal(@RequestBody Double amount, @PathVariable("id") Long id) {
+        return new ResponseEntity<>(goalService.updateGoal(amount, id) ? HttpStatus.NO_CONTENT : HttpStatus.CONFLICT);
+    }
 }
